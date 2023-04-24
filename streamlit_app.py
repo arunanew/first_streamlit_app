e61e92cf54ffe5ae631e64758f38f5668440fedb
arunanew <131330932+arunanew@users.noreply.github.com>
2023-04-24 14:40:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,7 @@ streamlit.title('my parents new health diner')
 streamlit.header('Breakfast Menu')
 
 streamlit.txt('Omega  & Blueberry oatmeal')
+
 streamlit.txt('Kale spinach & Rocket smoothie')
+
 streamlit.txt('Hard-Boild Free-Range Egg')
